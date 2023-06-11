08f402161bfb7373c7057702aeda796721bc837f
Gabriel Engel <gengel@gitlab.com>
2023-06-11 16:02:48 +0100

    Adding vimeo video

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-var player = new Vimeo.Player("video-background", {
-    url: "https://vimeo.com/835147882",
+var player = new Vimeo.Player('video-background', {
+    url: 'https://vimeo.com/835147882',
     background: true,
     autoplay: true,
     loop: true,
     byline: false,
     title: false,
-    portrait: false
-});
-
-window.addEventListener("scroll", function () {
-    var scrollingBox = document.getElementById("scrolling-box");
-
-    if (window.scrollY > 0) {
-        scrollingBox.classList.add("show");
-        player.pause();
-    } else {
-        scrollingBox.classList.remove("show");
-        player.play();
-    }
+    portrait: false,
+    muted: false // Add this line to enable sound
 });
